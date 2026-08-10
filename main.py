8950a7207fafa3dc41d1ed55df001f61f864a630
vovaantifreeze <vovaantifreeze@gmail.com>
2026-08-10 12:32:58 +0300

    Remove an active date from active_dates list
    Removed an active date from the list.

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ session = requests.Session()
 
 # --- Zile active ---
 active_dates = [
-    datetime(2026, 9, 11),
     datetime(2026, 8, 11),
     datetime(2026, 8, 12),
     datetime(2026, 8, 13),
